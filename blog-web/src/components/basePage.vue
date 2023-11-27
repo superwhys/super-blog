da26546350e2b560cb3976e30a715116ea774a96
superwhys <269085434@qq.com>
2023-11-27 11:58:02 +0800

    feat: add nav module for phone screen

--- blog-web/src/components/basePage.vue
+++ blog-web/src/components/basePage.vue
@@ -1,23 +1,8 @@
 <template>
   <div class="basePage">
     <el-container style="min-height: 100vh;">
-      <div class="header-menu" :style="{backgroundColor: backgroundColor}">
-        <div class="header-menu-left color-trans" :style="{color: navTextColor}">
-          SuperYong
-        </div>
-        <div class="header-menu-right">
-          <router-link to="/" :style="{color: navTextColor}" class="color-trans">HOME</router-link>
-          <router-link to="/about" :style="{color: navTextColor}" class="color-trans">ABOUT</router-link>
-          <router-link to="/tag" :style="{color: navTextColor}" class="color-trans">TAGS</router-link>
-        </div>
-        <div class="menu-icon" @click="toggleMobileNav" v-show="isMobile">
-          AAA
-        </div>
-        <div class="mobile-nav" v-if="showMobileNav">
-          <a href="">BBB</a>
-        </div>
-      </div>
       <el-header :style="backgroundImageStyle">
+        <nav-bar-module></nav-bar-module>
         <blog-header style="width: 100%; height: 100%">
           <template v-slot:headerImgInnerShow>
             <slot name="blogHeaderImgInnerShow"></slot>
@@ -46,10 +31,11 @@
 
 <script>
 import BlogHeader from "@/components/blogHeader.vue";
+import NavBarModule from "@/components/navBarModule.vue";
 
 export default {
   name: "basePage",
-  components: {BlogHeader},
+  components: {NavBarModule, BlogHeader},
   props: {
     showRight: {
       type: Boolean,
@@ -61,12 +47,7 @@ export default {
     }
   },
   data() {
-    return {
-      navTextColor: "white",
-      backgroundColor: "transparent",
-      isMobile: window.innerWidth < 760,
-      showMobileNav: false,
-    }
+    return {}
   },
   computed: {
     backgroundImageStyle() {
@@ -76,38 +57,6 @@ export default {
         backgroundImage: `url(${this.headerImgUrl})`
       }
     }
-  },
-  mounted() {
-    window.addEventListener('resize', this.handleResize);
-    this.handleResize();
-    window.addEventListener('scroll', this.changeColor);
-    if (window.innerWidth < 768) {
-      document.querySelectorAll('.header-menu-right').forEach(function(el) {
-        el.classList.add('no-animation');
-      });
-    }
-  },
-  beforeDestroy() {
-    window.removeEventListener('resize', this.handleResize);
-    window.removeEventListener('scroll', this.changeColor);
-  },
-  methods: {
-    handleResize() {
-      this.isMobile = window.innerWidth < 760;
-      console.log(this.isMobile)
-    },
-    toggleMobileNav() {
-      this.showMobileNav = !this.showMobileNav;
-    },
-    changeColor() {
-      if (window.scrollY > 400) {
-        this.backgroundColor = 'rgb(255,255,255, 0.8)';
-        this.navTextColor = 'black'
-      } else {
-        this.backgroundColor = 'transparent';
-        this.navTextColor = 'white'
-      }
-    },
   },
 }
 </script>
@@ -195,79 +144,6 @@ export default {
   }
 }
 
-.header-menu {
-  display: flex;
-  justify-content: space-between;
-  width: 100%;
-  height: 60px;
-  position: fixed;
-  z-index: 1000;
-  top: 0;
-  left: 0;
-  right: 0;
-  transition: background-color 0.5s;
-}
-
-.header-menu-left {
-  color: white;
-  margin: 10px 0 0 20px;
-  font-size: 30px;
-  font-weight: bold;
-  user-select: none;
-  display: flex;
-}
-
-.color-trans {
-  transition: color 0.5s;
-}
-
-.header-menu-right {
-  display: flex;
-  flex-direction: row;
-}
-
-@keyframes slideRightToHide {
-  0% {
-    clip-path: inset(0 0 0 0);
-  }
-  100% {
-    clip-path: inset(0 0 0 100%);
-  }
-}
-
-@keyframes slideLeftToAppear {
-  from { clip-path: inset(0 0 0 100%); }
-  to { clip-path: inset(0 0 0 0); }
-}
-
-@media (max-width: 768px) {
-  .header-menu-right {
-    animation: slideRightToHide 0.8s ease forwards;
-  }
-
-  .no-animation {
-    animation: none;
-    clip-path: inset(0 100% 0 0);
-  }
-}
-
-@media (min-width: 768px) {
-  .header-menu-right {
-    animation: slideLeftToAppear 0.8s ease forwards;
-  }
-}
-
-.header-menu-right a {
-  text-decoration: none;
-  color: white;
-  font-size: 12px;
-  font-weight: 900;
-  margin: 20px 20px 20px 20px;
-}
-
-.header-menu-right a:hover {
-  color: #d2cdcd;
-}
 
 .blogFooter {
   height: 200px;
@@ -276,21 +152,4 @@ export default {
   padding-bottom: 30px;
 }
 
-.menu-icon {
-  display: none;
-}
-.mobile-nav {
-  display: none;
-  /* 添加样式使导航栏从右侧弹出 */
-}
-@media (max-width: 760px) {
-  .menu-icon {
-    display: block;
-    /* 添加样式使图标显示 */
-  }
-  .mobile-nav {
-    display: block;
-    /* 添加样式使移动导航显示 */
-  }
-}
 </style>
---
+++ blog-web/src/components/navBarModule.vue
@@ -0,0 +1,270 @@
+<template>
+  <div class="header-menu" :style="{backgroundColor: backgroundColor}">
+    <div class="header-menu-left color-trans" :style="{color: navTextColor}">
+      SuperYong
+    </div>
+    <div class="menu-icon" :style="{color: navTextColor}" @click="toggleNav" v-if="isMobile">
+      <i class="el-icon-menu"></i>
+    </div>
+    <transition name="vertical-nav">
+      <div class="vertical-nav" v-show="showNav">
+        <div class="nav-header">
+          <div style="font-size: 18px; font-weight: bold">SuperYong</div>
+          <i class="el-icon-close" style="{color: #d6d6d6}" @click="toggleNav"></i>
+        </div>
+        <div class="spacer"></div>
+        <router-link
+            v-for="menu in menus"
+            :key="menu.name"
+            :to="menu.to"
+            @click.native="toggleNav"
+            :class="{isCurrentMenu: currentMenu === menu.to}"
+            class="navMenu"
+        >{{ menu.name }}</router-link>
+        <div class="spacer"></div>
+      </div>
+    </transition>
+    <div class="header-menu-right" v-if="!isMobile">
+      <router-link
+          v-for="menu in menus"
+          :key="menu.name"
+          :to="menu.to"
+          :style="{color: navTextColor}"
+          class="color-trans"
+      >{{ menu.name}}</router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "navBarModule",
+  data() {
+    return {
+      navTextColor: "white",
+      backgroundColor: "transparent",
+      mobileWidth: 576,
+      isMobile: window.innerWidth < this.mobileWidth,
+      showMobileNav: false,
+      showNav: false,
+      currentMenu: this.$route.path,
+      menus: [
+        {to: '/', name: 'HOME'},
+        {to: '/about', name: 'ABOUT'},
+        {to: '/tag', name: 'TAGS'},
+      ],
+    }
+  },
+  watch: {
+    $route(to, from) {
+      this.currentMenu = to.path;
+    }
+  },
+  methods: {
+    changeColor() {
+      if (window.scrollY > 400) {
+        this.backgroundColor = 'rgb(255,255,255, 0.8)';
+        this.navTextColor = 'black'
+      } else {
+        this.backgroundColor = 'transparent';
+        this.navTextColor = 'white'
+      }
+    },
+    handleResize() {
+      this.isMobile = window.innerWidth < this.mobileWidth;
+      console.log(this.isMobile)
+    },
+    toggleNav() {
+      this.showNav = !this.showNav;
+    },
+  },
+  mounted() {
+    window.addEventListener('scroll', this.changeColor);
+    window.addEventListener('resize', this.handleResize);
+    this.handleResize();
+
+    if (window.innerWidth < this.mobileWidth) {
+      document.querySelectorAll('.header-menu-right').forEach(function (el) {
+        el.classList.add('no-animation');
+      });
+    }
+  },
+  beforeDestroy() {
+    window.removeEventListener('scroll', this.changeColor);
+    window.removeEventListener('resize', this.handleResize);
+  }
+}
+</script>
+
+<style scoped>
+.header-menu {
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  height: 60px;
+  position: fixed;
+  z-index: 1000;
+  top: 0;
+  left: 0;
+  right: 0;
+  transition: background-color 0.5s;
+}
+
+.header-menu-left {
+  color: white;
+  margin: 10px 0 0 20px;
+  font-size: 30px;
+  font-weight: bold;
+  user-select: none;
+  display: flex;
+}
+
+.color-trans {
+  transition: color 0.5s;
+}
+
+.header-menu-right {
+  display: flex;
+  flex-direction: row;
+}
+
+@keyframes slideRightToHide {
+  0% {
+    clip-path: inset(0 0 0 0);
+  }
+  100% {
+    clip-path: inset(0 0 0 100%);
+  }
+}
+
+@keyframes slideLeftToAppear {
+  from {
+    clip-path: inset(0 0 0 100%);
+  }
+  to {
+    clip-path: inset(0 0 0 0);
+  }
+}
+
+.header-menu-right a {
+  text-decoration: none;
+  color: white;
+  font-size: 12px;
+  font-weight: 900;
+  margin: 20px 20px 20px 20px;
+}
+
+.header-menu-right a:hover {
+  color: #d2cdcd;
+}
+
+@media (max-width: 576px) {
+  .header-menu-right {
+    animation: slideRightToHide 0.8s ease forwards;
+  }
+
+  .no-animation {
+    animation: none;
+    clip-path: inset(0 100% 0 0);
+  }
+}
+
+@media (min-width: 576px) {
+  .header-menu-right {
+    animation: slideLeftToAppear 0.8s ease forwards;
+  }
+}
+
+.menu-icon {
+  display: none;
+}
+
+.vertical-nav {
+  display: none;
+}
+
+@media (max-width: 576px) {
+  .menu-icon {
+    display: flex;
+    align-items: center;
+    margin: 10px 20px 0 0;
+  }
+
+  .vertical-nav {
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: center;
+    position: fixed;
+    top: 0;
+    right: 0;
+    width: 60%;
+    height: 100vh;
+    background: #fff;
+    z-index: 1001;
+  }
+}
+
+.spacer {
+  flex-grow: 1;
+}
+
+.nav-header {
+  width: 80%;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  margin: 20px 20px 0 20px;
+}
+
+.navMenu {
+  text-decoration: none;
+  width: 60%;
+  color: black;
+  font-size: 13px;
+  font-weight: 900;
+  padding: 20px 20px 20px 20px;
+}
+
+.isCurrentMenu {
+  border: 1px solid var(--solidLineColor);
+  border-radius: 50px;
+}
+
+@keyframes slideInFromRight {
+  from {
+    transform: translateX(100%);
+  }
+  to {
+    transform: translateX(0);
+  }
+}
+
+@keyframes slideOutToRight {
+  from {
+    transform: translateX(0);
+  }
+  to {
+    transform: translateX(100%);
+  }
+}
+
+.vertical-nav-enter-active, .vertical-nav-leave-active {
+  animation-duration: 0.5s;
+}
+
+.vertical-nav-enter, .vertical-nav-leave-to
+{
+  transform: translateX(100%);
+}
+
+.vertical-nav-enter-active {
+  animation-name: slideInFromRight;
+}
+
+.vertical-nav-leave-active {
+  animation-name: slideOutToRight;
+}
+
+</style>
